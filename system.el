0bac3f484c0bc2a6696c47332491cb753ab427cf
Randy Ridenour <rlridenour@Randys-MacBook-Pro.local>
2015-11-05 06:49:18 -0600

    Added latex.el

--- system.el
+++ system.el
@@ -144,29 +144,6 @@
 
 
 
-;; == LaTex / AucTeX ==
-(use-package tex
-  :defer t
-  :ensure auctex
-  :config
-  (setq TeX-auto-save t)
-  (setq TeX-parse-self t)
-  (setq-default TeX-master nil)
-  (add-hook 'LaTeX-mode-hook 'visual-line-mode)
-  (add-hook 'LaTeX-mode-hook 'flyspell-mode)
-  (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
-  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-  (setq reftex-plug-into-AUCTeX t)
-  (setq TeX-PDF-mode t)
-  (setq reftex-bibliography-commands '("bibliography" "nobibliography" "addbibresource"))
-  
-  ;; Don't use Helm for the reftex-citation lookup
-;;  (eval-after-load 'helm-mode
-;;    '(add-to-list 'helm-completing-read-handlers-alist '(reftex-citation . nil))
-;;    )
-  
-  )
-
 
 
 
