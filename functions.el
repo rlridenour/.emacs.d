fbdab63bef86d75f43e012efd70ea23fff4bc7fe
rlridenour <rlridenour@gmail.com>
2016-02-05 11:16:40 -0600

    Add unfill-paragraph.

--- functions.el
+++ functions.el
@@ -331,3 +331,10 @@ Version 2015-05-07"
   (interactive)
   (occur "[^[:ascii:]]"))
 
+;; Unfill paragraph
+;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph    
+    (defun unfill-paragraph (&optional region)
+      "Takes a multi-line paragraph and makes it into a single line of text."
+      (interactive (progn (barf-if-buffer-read-only) '(t)))
+      (let ((fill-column (point-max)))
+        (fill-paragraph nil region)))
