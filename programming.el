a73d69072dbbcbfdac0be62cff1ebcbf02adbf22
Randy Ridenour <rlridenour@Randys-MacBook-Pro.local>
2016-06-04 16:25:46 -0500

    Add racket-mode.

--- programming.el
+++ programming.el
@@ -13,3 +13,14 @@
   (elpy-enable))
 
 
+;;;;;;;;;;;;
+;; Racket ;;
+;;;;;;;;;;;;
+
+(use-package racket-mode
+  :ensure t
+  :defer t
+  :mode ("\\.rkt[dl]?\\'" . racket-mode))
+
+;; (use-package geiser
+;; :ensure t)
