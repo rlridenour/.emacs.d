f6fead1d2b2a6698e410b9974568227efd109dc1
Randy Ridenour <rlridenour@MBP.local>
2016-11-17 07:09:28 -0600

    Add web-mode

--- utilities.el
+++ utilities.el
@@ -214,3 +214,16 @@
   :config
   (global-hungry-delete-mode))
 
+;; Web-mode (from http://cestlaz.github.io)
+(use-package web-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (setq web-mode-engines-alist
+		'(("django"    . "\\.html\\'")))
+  (setq web-mode-ac-sources-alist
+		'(("css" . (ac-source-css-property))
+		  ("html" . (ac-source-words-in-buffer ac-source-abbrev))))
+
+  (setq web-mode-enable-auto-closing t)
+  (setq web-mode-enable-auto-quoting t)) ; this fixes the quote problem I mentioned
